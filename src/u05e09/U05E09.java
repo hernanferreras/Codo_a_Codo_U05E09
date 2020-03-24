983953971e3994e4e94376c5cabdc19cccc27da1
hernan <hernan@Dagobah>
2020-03-24 18:51:50 -0300

    Unidad 5 Ejercicio 9 de la practica de Codo a Codo

---
+++ src/u05e09/U05E09.java
@@ -0,0 +1,42 @@
+
+package u05e09;
+
+import java.util.Scanner;
+
+public class U05E09 {
+
+    public static void main(String args[]) {
+        Scanner entrada=new Scanner(System.in);
+        char vocal;
+        boolean x = true;
+        System.out.print("Ingrese una vocal: ");
+        vocal=entrada.next().charAt(0);
+        vocal=Character.toUpperCase(vocal);
+        x=esVocal(vocal);
+        System.out.print(x);
+    }
+    static boolean esVocal(char a){
+        boolean z;
+        switch(a){
+            case 'A':
+                z=true;
+                break;
+            case 'E':
+                z=true;
+                break;
+            case 'I':
+                z=true;
+                break;
+            case 'O':
+                z=true;
+                break;
+            case 'U':
+                z=true;
+                break;
+            default:
+                z=false;
+                break;
+        }
+    return(z);
+    }
+}
